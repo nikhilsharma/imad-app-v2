fc2c0ce3788b8e6be7a732f432cdf2b6c65f93de
nikhilsharma <nikhil.sharma_26@yahoo.com>
2017-02-19 17:36:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,21 +2,44 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 
-
-var articleOne = {
-  title:'Article One | Nikhil Sharma',
-  heading:'Article One',
-  date:'Feb 19th 2017',
-  content: `<p>
-            This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
-            </p>
-            <p>
-            This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
-            </p>
-            <p>
-            This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
-            </p>`
+var articles = {
+    articleOne : {
+        title:'Article One | Nikhil Sharma',
+          heading:'Article One',
+          date:'Feb 19th 2017',
+          content: `<p>
+                    This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
+                    </p>
+                    <p>
+                    This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
+                    </p>
+                    <p>
+                    This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
+                    </p>`
+    },
+    
+    articleTwo : {
+        title:'Article Two | Nikhil Sharma',
+          heading:'Article Two',
+          date:'Feb 20th 2017',
+          content: `<p>
+                    This is the content for my article. 
+                    </p>
+                    `
+    },
+    
+    articleThree : {
+        title:'Article Three | Nikhil Sharma',
+          heading:'Article Three',
+          date:'Feb 19th 2017',
+          content: `<p>
+                    This is the content for my article.
+                    </p>
+                    `
+    }
 };
+
+
 
 function createTemplate (data) {
     var title = data.title;
@@ -65,8 +88,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function(req, res) {
-   res.send(createTemplate(articleOne)); 
+app.get('/:articleName', function(req, res) {
+    // articleName == article-one
+    
+   res.send(createTemplate(articles[articleName])); 
 });
 
 app.get('/article-two', function(req, res) {
